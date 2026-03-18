ead2bd275b3777800719d15283e46b9ace9d41f8
lfherreragaray <lf.herreragaray@gmail.com>
2026-03-18 15:27:55 -0600

    ya duerman al caballo

--- src/main/java/com/example/javaaxity_git_java/JavaaxityGitJavaApplication.java
+++ src/main/java/com/example/javaaxity_git_java/JavaaxityGitJavaApplication.java
@@ -9,6 +9,10 @@ public class JavaaxityGitJavaApplication {
 	public String saludar(){
 		return "Pohita te quiero";
 	}
+
+	public String rollingeyes(){
+		return "que curso tan bodrio";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaaxityGitJavaApplication.class, args);
 	}
