ae9ad5784cc639bf70bf2999349a3c75969494ce
VeronicaRG-asamitaka <veronica.ruizgalvan@hotmail.com>
2026-03-18 14:44:09 -0600

    Se agregan archivos del proyecto java

---
+++ src/main/java/com/example/javaaxity_git_java/JavaaxityGitJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaaxity_git_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaaxityGitJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaaxityGitJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaaxity_git_java/JavaaxityGitJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaaxity_git_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaaxityGitJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
